6f15742f8818d2bf9b3f9e68e92066192909a27f
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-12-09 20:22:37 -0300

    trabalhando impressao de variáveis

---
+++ impressao/impressaoVariaveis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void main (){
+    char nome_da_variavel1 = 'A';
+    int nome_da_variavel3 = 10;
+    long int nome_da_variavel4 = 20;
+    unsigned short int nome_da_variavel6 = 30;
+    unsigned int nome_da_variavel2 = 40;
+    float nome_da_variavel5 = 50.5;
+    double nome_da_variavel7 = 60.6;
+    long double nome_da_variavel8 = 70.7;
+    const int maximo = 100;
+    
+    printf("imprimido o valor das variaveid: %c\n %d\n %d\n %d\n %d\n %f\n %f\n %\nf %e\n", nome_da_variavel1, nome_da_variavel3, nome_da_variavel4, nome_da_variavel1, nome_da_variavel2, nome_da_variavel5, nome_da_variavel6, nome_da_variavel8);
+} 
