f273aeb9b8b5c82d6cd0026e0abcac6e2a43a3c2
Jose Lima <recalculus@recalculus.com.br>
2026-06-13 20:04:26 -0300

    adiciondo operaor ternario

--- calculocomcientifico/entrada.c
+++ calculocomcientifico/entrada.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<windows.h> // Necessário para SetConsoleOutputCP
 
 int main(){
     int idade;
     float altura;
     char letra_favorita;
     char nome[20];
+    
+    SetConsoleOutputCP(CP_UTF8); // Define a codificação de saída do console para UTF-8
 
     printf("Olá Mundo!\n");
     printf("Digite seu nome: ");
@@ -14,7 +17,11 @@ int main(){
     printf("Qual a sua altura?  ");
     scanf("%f", &altura);
     printf("Qual a sua letra favorita?");
-    scanf("%c", &letra_favorita);
+    scanf(" %c", &letra_favorita);
+
+    printf("A sua idade é: %i\n"
+           "A sua altura é: %.2f\n"
+           "A sua letra favorita é: %c\n", idade, altura, letra_favorita);
 
     return 0;
 }
