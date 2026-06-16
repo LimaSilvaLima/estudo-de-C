09aa844bf156584f1d941f1d33852fb9fb3a7715
Jose Lima <recalculus@recalculus.com.br>
2026-06-16 16:15:40 -0300

    case foi inserido no loop do while

--- estruturascase/casemini/minicase.c
+++ estruturascase/casemini/minicase.c
@@ -3,17 +3,28 @@
 
 int main(){
    SetConsoleOutputCP(CP_UTF8);
-    int opcao;
-    printf("Digite 1 ou 2:");
-    scanf("%d", &opcao);    
-    switch (opcao){
-        case 1 :
-        printf("Voce escolheu a opção 1");
-        break;
-        case 2 :
-        printf("Voce escolheu a opção 2");
-            break;
-        default:
-        printf("\n Voce não digitou uma Opção inválida");
-    }
+   int opcao;
+
+   do {
+       printf("Digite 1 ou 2: ");
+       if (scanf("%d", &opcao) != 1) {
+           printf("\nErro: Por favor, digite um numero inteiro.\n");
+           while (getchar() != '\n'); // Limpa o buffer para evitar loop infinito
+           opcao = 0; // Define um valor que mantém o loop ativo
+           continue;
+       }
+
+       switch (opcao) {
+           case 1:
+               printf("Voce escolheu a opção 1\n");
+               break;
+           case 2:
+               printf("Voce escolheu a opção 2\n");
+               break;
+           default:
+               printf("\nOpção inválida! Tente novamente.\n");
+       }
+   } while (opcao != 1 && opcao != 2);
+
+   return 0;
 }
